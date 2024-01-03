ec92288a572fff8477a1b82b94f6417520d1273b
sonhoang3000 <vuhoangson3000@gmail.com>
2024-01-03 11:01:10 +0700

    #37 Part1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,29 +1,49 @@
 import userService from "../services/userService";
 
 let handleLogin = async (req, res) => {
-  let email = req.body.email;
-  let password = req.body.password;
+	let email = req.body.email;
+	let password = req.body.password;
 
-  if (!email || !password) {
-    return res.status(500).json({
-      errCode: 1,
-      message: "Missing inputs parameters",
-    });
-  }
+	if (!email || !password) {
+		return res.status(500).json({
+			errCode: 1,
+			message: "Missing inputs parameters",
+		});
+	}
 
-  let userData = await userService.handleUserLogin(email, password);
-  console.log(userData);
-  // check mail exist
-  // compare password
-  // return userinfor
-  // access token
-  return res.status(200).json({
-    errCode: userData.errCode,
-    message: userData.errMessage,
-    user: userData.user ? userData.user : {},
-  });
+	let userData = await userService.handleUserLogin(email, password);
+	console.log(userData);
+	// check mail exist
+	// compare password
+	// return userinfor
+	// access token
+	return res.status(200).json({
+		errCode: userData.errCode,
+		message: userData.errMessage,
+		user: userData.user ? userData.user : {},
+	});
 };
 
+let handleGetAllUsers = async (req, res) => {
+	let id = req.body.id //ALL, id
+
+	if (!id) {
+		return res.status(200).json({
+			errCode: 1,
+			errMessage: "Missing required parameters",
+			users: []
+		})
+	}
+
+	let users = await userService.getAllUsers(id);
+	return res.status(200).json({
+		errCode: 0,
+		errMessage: "I'am Okie, I'm fine, kin cha na",
+		users
+	})
+}
+
 module.exports = {
-  handleLogin: handleLogin,
+	handleLogin: handleLogin,
+	handleGetAllUsers: handleGetAllUsers,
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,66 +1,97 @@
 import db from "../models/index";
 import bcrypt from "bcryptjs";
+import { resolve } from "path";
+import e from "express";
 
 let handleUserLogin = (email, password) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      let userData = {};
-      let isExist = await checkUserEmail(email);
-      if (isExist) {
-        //user already exist
+	return new Promise(async (resolve, reject) => {
+		try {
+			let userData = {};
+			let isExist = await checkUserEmail(email);
+			if (isExist) {
+				//user already exist
 
-        let user = await db.User.findOne({
-          attributes: ["email", "roleId", "password"],
-          where: { email: email },
-          raw: true,
-        });
-        if (user) {
-          //compare password
-          let check = await bcrypt.compareSync(password, user.password);
-          if (check) {
-            userData.errCode = 0;
-            userData.errMessage = "Okie nhe";
+				let user = await db.User.findOne({
+					attributes: ["email", "roleId", "password"],
+					where: { email: email },
+					raw: true,
+				});
+				if (user) {
+					//compare password
+					let check = await bcrypt.compareSync(password, user.password);
+					if (check) {
+						userData.errCode = 0;
+						userData.errMessage = "Okie nhe";
 
-            delete user.password;
-            userData.user = user;
-          } else {
-            userData.errCode = 3;
-            userData.errMessage = "Wrong password";
-          }
-        } else {
-          userData.errCode = 2;
-          userData.errMessage = `User's not found`;
-        }
-      } else {
-        //return error
-        userData.errCode = 1;
-        userData.errMessage = `Your email or your password isn't exist in your system. Plz try other email`;
-      }
+						delete user.password;
+						userData.user = user;
+					} else {
+						userData.errCode = 3;
+						userData.errMessage = "Wrong password";
+					}
+				} else {
+					userData.errCode = 2;
+					userData.errMessage = `User's not found`;
+				}
+			} else {
+				//return error
+				userData.errCode = 1;
+				userData.errMessage = `Your email or your password isn't exist in your system. Plz try other email`;
+			}
 
-      resolve(userData);
-    } catch (e) {
-      reject(e);
-    }
-  });
+			resolve(userData);
+		} catch (e) {
+			reject(e);
+		}
+	});
 };
 
 let checkUserEmail = (userEmail) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      let user = await db.User.findOne({
-        where: { email: userEmail },
-      });
-      if (user) {
-        resolve(true);
-      } else {
-        resolve(false);
-      }
-    } catch (e) {
-      reject(e);
-    }
-  });
+	return new Promise(async (resolve, reject) => {
+		try {
+			let user = await db.User.findOne({
+				where: { email: userEmail },
+			});
+			if (user) {
+				resolve(true);
+			} else {
+				resolve(false);
+			}
+		} catch (e) {
+			reject(e);
+		}
+	});
 };
 
+let getAllUsers = (userId) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			let users = 'abcxyz';
+			if (userId === 'ALL') {
+				users = await db.User.findAll({
+					attributes: {
+						exclude: ['password']
+					}
+				})
+			}
+			if (userId && userId !== 'ALL') {
+				users = await db.User.findOne({
+					where: { id: userId },
+					attributes: {
+						exclude: ['password']
+					}
+				})
+			}
+			resolve(users)
+
+		} catch (e) {
+			reject(e)
+		}
+
+	})
+}
+
 module.exports = {
-  handleUserLogin: handleUserLogin,
+	handleUserLogin: handleUserLogin,
+	getAllUsers: getAllUsers,
 };
