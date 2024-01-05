aedd1fc599772f5dd1764d994b12ce929a5f0412
sonhoang3000 <vuhoangson3000@gmail.com>
2024-01-05 12:04:48 +0700

    #41 create

--- src/services/userService.js
+++ src/services/userService.js
@@ -108,28 +108,30 @@ let createNewUser = (data) => {
 		try {
 			// check email is exist ???
 			let check = await checkUserEmail(data.email);
-			if (check) {
+			if (check === true) {
 				resolve({
 					errCode: 1,
-					message: "Your email is already used, Please try another email!"
+					errMessage: "Your email is already used, Please try another email!"
+				});
+			} else {
+				let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+				await db.User.create({
+					email: data.email,
+					password: hashPasswordFromBcrypt,
+					firstName: data.firstName,
+					lastName: data.lastName,
+					address: data.address,
+					phonenumber: data.phonenumber,
+					gender: data.gender === "1" ? true : false,
+					roleId: data.roleId,
+				});
+				resolve({
+					errCode: 0,
+					message: "Okie kin chana"
 				});
 			}
 
-			let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-			await db.User.create({
-				email: data.email,
-				password: hashPasswordFromBcrypt,
-				firstName: data.firstName,
-				lastName: data.lastName,
-				address: data.address,
-				phonenumber: data.phonenumber,
-				gender: data.gender === "1" ? true : false,
-				roleId: data.roleId,
-			});
-			resolve({
-				errCode: 0,
-				message: "Okie con kin chana"
-			});
+
 		} catch (e) {
 			reject(e)
 		}
