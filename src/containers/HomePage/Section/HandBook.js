d64cce417336e75090d40862ca779a4f95e4cfc4
sonhoang3000 <vuhoangson3000@gmail.com>
2024-01-10 21:59:43 +0700

    #50

--- src/containers/HomePage/Section/HandBook.js
+++ src/containers/HomePage/Section/HandBook.js
@@ -1,47 +1,43 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import "./Specialty.scss";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { FormattedMessage } from "react-intl";
 import Slider from "react-slick";
 
-
-class Specialty extends Component {
-
+class HandBook extends Component {
 
 	render() {
-
 		return (
-			<div className="section-share section-specialty">
+			<div className="section-share section-handbook ">
 				<div className="section-container">
 					<div className="section-header">
-						<span className="title-section">Chuyên khoa phổ biến</span>
+						<span className="title-section">Cẩm nang</span>
 						<button className="btn-section">xem thêm</button>
 					</div>
 
 					<div className="section-body">
 						<Slider {...this.props.settings}>
 							<div className="section-customize">
-								<div className="bg-image section-specialty"></div>
+								<div className="bg-image section-handbook"></div>
 								<div>Cơ xương khớp 1</div>
 							</div>
 							<div className="section-customize">
-								<div className="bg-image section-specialty"></div>
+								<div className="bg-image section-handbook"></div>
 								<div>Cơ xương khớp 2</div>
 							</div>
 							<div className="section-customize">
-								<div className="bg-image section-specialty"></div>
+								<div className="bg-image section-handbook"></div>
 								<div>Cơ xương khớp 3</div>
 							</div>
 							<div className="section-customize">
-								<div className="bg-image section-specialty"></div>
+								<div className="bg-image section-handbook"></div>
 								<div>Cơ xương khớp 4</div>
 							</div>
 							<div className="section-customize">
-								<div className="bg-image section-specialty"></div>
+								<div className="bg-image section-handbook"></div>
 								<div>Cơ xương khớp 5</div>
 							</div>
 							<div className="section-customize">
-								<div className="bg-image section-specialty"></div>
+								<div className="bg-image section-handbook"></div>
 								<div>Cơ xương khớp 6</div>
 							</div>
 
@@ -50,20 +46,21 @@ class Specialty extends Component {
 				</div>
 			</div >
 		)
+
+
 	}
 
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
 	return {
-		isLoggedIn: state.user.isLoggedIn,
-		language: state.app.language,
+		isLoggedIn: state.user.isLoggedIn
 	};
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
 	return {
 	};
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
+export default connect(mapStateToProps, mapDispatchToProps)(HandBook);
