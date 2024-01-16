2ba3f505cb30ef75b357645f7f317b2333eb9500
sonhoang3000 <vuhoangson3000@gmail.com>
2024-01-17 01:25:02 +0700

    #66

--- src/containers/HomePage/Section/OutStandingDoctor.js
+++ src/containers/HomePage/Section/OutStandingDoctor.js
@@ -28,13 +28,17 @@ class OutStandingDoctor extends Component {
 	render() {
 		let arrDoctors = this.state.arrDoctors;
 		let { language } = this.props;
-		arrDoctors = arrDoctors.concat(arrDoctors).concat(arrDoctors)
+		// arrDoctors = arrDoctors.concat(arrDoctors).concat(arrDoctors)
 		return (
 			<div className="section-share section-outstanding-doctor ">
 				<div className="section-container">
 					<div className="section-header">
-						<span className="title-section">Bác sĩ nổi bật</span>
-						<button className="btn-section">xem thêm</button>
+						<span className="title-section">
+							<FormattedMessage id="homepage.outstanding-doctor" />
+						</span>
+						<button className="btn-section">
+							<FormattedMessage id="homepage.more-info" />
+						</button>
 					</div>
 
 					<div className="section-body">
