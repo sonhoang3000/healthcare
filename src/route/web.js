9d2e88e461fe34af4dc6e3553a959b935735c0e2
sonhoang3000 <vuhoangson3000@gmail.com>
2024-01-18 02:46:05 +0700

    #68

--- src/route/web.js
+++ src/route/web.js
@@ -13,7 +13,6 @@ let initWebRoutes = (app) => {
   router.post("/post-crud", homeController.postCRUD);
   router.get("/get-crud", homeController.displayGetCRUD);
   router.get("/edit-crud", homeController.getEditCRUD);
-
   router.post("/put-crud", homeController.putCRUD);
   router.get("/delete-crud", homeController.deleteCRUD);
 
@@ -22,13 +21,12 @@ let initWebRoutes = (app) => {
   router.post("/api/create-new-users", userController.handleCreateNewUser)
   router.put("/api/edit-user", userController.handleEditUser)
   router.delete("/api/delete-user", userController.handleDeleteUser) // rest API
-
   router.get('/api/allcode', userController.getAllCode);
 
   router.get('/api/top-doctor-home', doctorController.getTopDoctorHome);
-
   router.get('/api/get-all-doctors', doctorController.getAllDoctors);
   router.post('/api/save-infor-doctors', doctorController.postInforDoctor);
+  router.get('/api/get-detail-doctor-by-id', doctorController.getDetailDoctorById);
 
 
 
