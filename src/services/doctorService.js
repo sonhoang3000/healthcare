4ce0f3049a7c9e54ec285d1081e87ed2cb9b838e
sonhoang3000 <vuhoangson3000@gmail.com>
2024-01-18 12:40:22 +0700

    #69

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -91,7 +91,7 @@ let getDetailDoctorById = (inputId) => {
 						id: inputId
 					},
 					attributes: {
-						exclude: ['password', 'image']
+						exclude: ['password']
 					},
 					include: [
 						{
@@ -101,9 +101,15 @@ let getDetailDoctorById = (inputId) => {
 
 						{ model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
 					],
-					raw: true,
+					raw: false,
 					nest: true
 				})
+
+				if (data && data.image) {
+					data.image = new Buffer(data.image, 'base64').toString('binary');
+				}
+
+				if (!data) data = {};
 
 				resolve({
 					errCode: 0,
