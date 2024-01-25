9f7a74a285febd031ad9b19447c6850962e5d555
sonhoang3000 <vuhoangson3000@gmail.com>
2024-01-26 00:52:44 +0700

    #76 #77

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -171,17 +171,9 @@ let bulkCreateSchedule = (data) => {
 					}
 				);
 
-				// convert date
-				if (existing && existing.length > 0) {
-					existing = existing.map(item => {
-						item.date = new Date(item.date).getTime();
-						return item;
-					})
-				}
-
 				//compare differenceWith
 				let toCreate = _.differenceWith(schedule, existing, (a, b) => {
-					return a.timeType === b.timeType && a.date === b.date;
+					return a.timeType === b.timeType && +a.date === +b.date;
 				});
 
 				//create data
@@ -216,7 +208,12 @@ let getScheduleByDate = (doctorId, date) => {
 					where: {
 						doctorId: doctorId,
 						date: date
-					}
+					},
+					include: [
+						{ model: db.Allcode, as: 'timeTypeData', attributes: ['valueEn', 'valueVi'] },
+					],
+					raw: false,
+					nest: true
 				})
 
 				if (!dataSchedule) dataSchedule = [];
