46f9c175b7126446f7635cb49ca1e86284b50780
sonhoang3000 <vuhoangson3000@gmail.com>
2024-01-25 12:38:19 +0700

    #75

--- src/containers/Patient/Doctor/DetailDoctor.js
+++ src/containers/Patient/Doctor/DetailDoctor.js
@@ -4,7 +4,7 @@ import HomeHeader from '../../HomePage/HomeHeader';
 import './DetailDoctor.scss';
 import { getDetailInforDoctor } from '../../../services/userService';
 import { LANGUAGES } from '../../../utils';
-
+import DoctorSchedule from './DoctorSchedule';
 class DetailDoctor extends Component {
 
       constructor(props) {
@@ -31,7 +31,6 @@ class DetailDoctor extends Component {
       }
 
       render() {
-            console.log('this.state check: ', this.state)
             let { language } = this.props;
 
             let { detailDoctor } = this.state;
@@ -69,7 +68,14 @@ class DetailDoctor extends Component {
                                     </div>
                               </div>
                               <div className='schedule-doctor'>
+                                    <div className='content-left'>
+                                          <DoctorSchedule
+                                                doctorIdFromParent={detailDoctor && detailDoctor.id ? detailDoctor.id : -1}
+                                          />
+                                    </div>
+                                    <div className='content-right'>
 
+                                    </div>
                               </div>
                               <div className='detail-infor-doctor'>
                                     {detailDoctor && detailDoctor.Markdown && detailDoctor.Markdown.contentHTML
