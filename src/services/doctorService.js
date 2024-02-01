acb659ff47d32c8f621ca5b897003dcea2b23a4a
sonhoang3000 <vuhoangson3000@gmail.com>
2024-02-01 15:35:18 +0700

    #78 #79

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,7 +1,6 @@
 import db from "../models/index";
 require('dotenv').config();
 import _ from 'lodash';
-
 const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE;
 
 let getTopDoctorHome = (limitInput) => {
@@ -57,13 +56,22 @@ let getAllDoctors = () => {
 let saveDetailInforDoctor = (inputData) => {
 	return new Promise(async (resolve, reject) => {
 		try {
-			if (!inputData.doctorId || !inputData.contentHTML
-				|| !inputData.contentMarkdown || !inputData.action) {
+
+			if (!inputData.doctorId
+				|| !inputData.contentHTML
+				|| !inputData.contentMarkdown || !inputData.action
+				|| !inputData.selectedPrice || !inputData.selectedPayment
+				|| !inputData.selectedProvince
+				|| !inputData.nameClinic || !inputData.addressClinic
+				|| !inputData.note
+			) {
 				resolve({
 					errCode: 1,
 					errMessage: 'Missing parameters'
 				})
 			} else {
+
+				// upsert to Markdown 
 				if (inputData.action === 'CREATE') {
 					await db.Markdown.create({
 						contentHTML: inputData.contentHTML,
@@ -86,7 +94,36 @@ let saveDetailInforDoctor = (inputData) => {
 					}
 				}
 
+				// upsert to Doctor_infor table
+				let doctorInfor = await db.Doctor_Infor.findOne({
+					where: {
+						doctorId: inputData.doctorId,
+					},
+					raw: false
+				})
+				if (doctorInfor) {
+					//update
+					doctorInfor.doctorId = inputData.doctorId;
+					doctorInfor.priceId = inputData.selectedPrice;
+					doctorInfor.provinceId = inputData.selectedProvince;
+					doctorInfor.paymentId = inputData.selectedPayment;
+					doctorInfor.nameClinic = inputData.nameClinic;
+					doctorInfor.addressClinic = inputData.addressClinic;
+					doctorInfor.note = inputData.note;
+					await doctorInfor.save()
 
+				} else {
+					//create
+					await db.Doctor_Infor.create({
+						doctorId: inputData.doctorId,
+						priceId: inputData.selectedPrice,
+						provinceId: inputData.selectedProvince,
+						paymentId: inputData.selectedPayment,
+						nameClinic: inputData.nameClinic,
+						addressClinic: inputData.addressClinic,
+						note: inputData.note,
+					})
+				}
 
 				resolve({
 					errCode: 0,
