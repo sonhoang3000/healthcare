16544e76ec0214e031e969c6a7129b9948b8a270
sonhoang3000 <vuhoangson3000@gmail.com>
2024-02-04 11:26:32 +0700

    #84 #85

--- src/containers/Patient/Doctor/ProfileDoctor.js
+++ src/containers/Patient/Doctor/ProfileDoctor.js
@@ -5,6 +5,8 @@ import './ProfileDoctor.scss';
 import { getProfileDoctorById } from '../../../services/userService';
 import { LANGUAGES } from '../../../utils';
 import NumberFormat from 'react-number-format';
+import _ from 'lodash';
+import moment from 'moment';
 
 class ProfileDoctor extends Component {
 
@@ -45,9 +47,30 @@ class ProfileDoctor extends Component {
 
 	}
 
+	renderTimeBooking = (dataTime) => {
+		let { language } = this.props;
+		if (dataTime && !_.isEmpty(dataTime)) {
+			let time = language === LANGUAGES.VI ?
+				dataTime.timeTypeData.valueVi : dataTime.timeTypeData.valueEn;
+
+			let date = language === LANGUAGES.VI ?
+				moment.unix(+dataTime.date / 1000).format('dddd - DD/MM/YYYY')
+				:
+				moment.unix(+dataTime.date / 1000).locale('en').format('ddd - MM/DD/YYYY')
+			return (
+				<>
+					<div>{time}  {date}</div>
+					<div><FormattedMessage id="patient.booking-modal.priceBooking" /></div>
+				</>
+			)
+		}
+		return <></>
+
+	}
+
 	render() {
 		let { dataProfile } = this.state
-		let { language } = this.props
+		let { language, isShowDescriptionDoctor, dataTime } = this.props
 
 		let nameVi = '', nameEn = '';
 		if (dataProfile && dataProfile.positionData) {
@@ -67,19 +90,27 @@ class ProfileDoctor extends Component {
 							{language === LANGUAGES.VI ? nameVi : nameEn}
 						</div>
 						<div className='down'>
-							{dataProfile && dataProfile.Markdown
-								&& dataProfile.Markdown.description
-								&&
-								<span>
-									{dataProfile.Markdown.description}
-								</span>
+							{isShowDescriptionDoctor === true ?
+								<>
+									{dataProfile && dataProfile.Markdown
+										&& dataProfile.Markdown.description
+										&&
+										<span>
+											{dataProfile.Markdown.description}
+										</span>
+									}
+								</>
+								:
+								<>
+									{this.renderTimeBooking(dataTime)}
+								</>
 							}
 						</div>
 					</div>
 
 				</div>
 				<div className='price'>
-					Giá khám :
+					<FormattedMessage id="patient.booking-modal.price" />
 					{dataProfile && dataProfile.Doctor_Infor && language === LANGUAGES.VI &&
 						<NumberFormat
 							className='currency'
--- src/services/userService.js
+++ src/services/userService.js
@@ -63,6 +63,10 @@ const getProfileDoctorById = (doctorId) => {
   return axios.get(`/api/get-profile-doctor-by-id?doctorId=${doctorId}`)
 }
 
+const postPatientBookingAppointment = (data) => {
+  return axios.post('/api/patient-book-appointment', data)
+}
+
 export {
   handleLoginApi, getAllUsers,
   createNewUserService, deleteUserService,
@@ -71,5 +75,5 @@ export {
   saveDetailDoctorService,
   getDetailInforDoctor, saveBulkScheduleDoctor,
   getScheduleDoctorByDate, getExtraInforDoctorById,
-  getProfileDoctorById
+  getProfileDoctorById, postPatientBookingAppointment
 };
