718b673f6791ece2933a2f4f86404ed9a5e7d40f
sonhoang3000 <vuhoangson3000@gmail.com>
2024-02-05 00:13:30 +0700

    #86 #87

--- src/services/patientService.js
+++ src/services/patientService.js
@@ -1,15 +1,29 @@
 import db from "../models/index";
 require('dotenv').config();
+import emailService from "./emailService"
 
 let postBookAppointment = (data) => {
 	return new Promise(async (resolve, reject) => {
 		try {
-			if (!data.email || !data.doctorId || !data.timeType || !data.date) {
+			if (!data.email || !data.doctorId || !data.timeType || !data.date
+				|| !data.fullName
+
+			) {
 				resolve({
 					errCode: 1,
 					errMessage: 'Missing parameters'
 				})
 			} else {
+
+				await emailService.sendSimpleEmail({
+					reciversEmail: data.email,
+					patientName: data.fullName,
+					time: data.timeString,
+					doctorName: data.doctorName,
+					language: data.language,
+					redirectLink: "https://bookingcare.vn/"
+				})
+
 				// upsert patient
 				let user = await db.User.findOrCreate({
 					where: { email: data.email },
