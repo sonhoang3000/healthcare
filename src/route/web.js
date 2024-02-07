b4249fd6be3407b1908f19b491ca745de07afc0c
sonhoang3000 <vuhoangson3000@gmail.com>
2024-02-07 22:10:42 +0700

    #96

--- src/route/web.js
+++ src/route/web.js
@@ -4,7 +4,7 @@ import userController from "../controllers/userController";
 import doctorController from "../controllers/doctorController";
 import patientController from "../controllers/patientController";
 import specialtyController from "../controllers/specialtyController";
-
+import clinicController from "../controllers/clinicController";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
@@ -40,6 +40,9 @@ let initWebRoutes = (app) => {
   router.get('/api/get-specialty', specialtyController.getAllSpecialty);
   router.get('/api/get-detail-specialty-by-id', specialtyController.getDetailSpecialtyById);
 
+  router.post('/api/create-new-clinic', clinicController.createClinic);
+  // router.get('/api/get-specialty', specialtyController.getAllSpecialty);
+  // router.get('/api/get-detail-specialty-by-id', specialtyController.getDetailSpecialtyById);
 
   return app.use("/", router);
 };
---
+++ src/services/clinicService.js
@@ -0,0 +1,35 @@
+const db = require("../models")
+
+let createClinic = (data) => {
+      return new Promise(async (resolve, reject) => {
+            try {
+                  if (!data.name || !data.imageBase64 || !data.descriptionHTML ||
+                        !data.descriptionMarkdown || !data.address) {
+                        resolve({
+                              errCode: 1,
+                              errMessage: 'Missing parameters'
+                        })
+                  } else {
+
+                        await db.Clinic.create({
+                              name: data.name,
+                              address: data.address,
+                              image: data.imageBase64,
+                              descriptionHTML: data.descriptionHTML,
+                              descriptionMarkdown: data.descriptionMarkdown
+                        })
+
+                        resolve({
+                              errCode: 0,
+                              errMessage: 'OK'
+                        })
+                  }
+            } catch (e) {
+                  reject(e)
+            }
+      })
+}
+
+module.exports = {
+      createClinic: createClinic
+}
