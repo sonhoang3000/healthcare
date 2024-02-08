f5be587fc6c44a602d9e037e492073e47b55bdce
sonhoang3000 <vuhoangson3000@gmail.com>
2024-02-09 00:29:10 +0700

    #98

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -1,6 +1,5 @@
 export const adminMenu = [
     { //Quản lý người dùng
-
         name: 'menu.admin.manage-user',
         menus: [
             {
@@ -54,13 +53,15 @@ export const adminMenu = [
     },
 ];
 
-
 export const doctorMenu = [
     {
         name: 'menu.admin.manage-user',
         menus: [
             { //Quản lý kế hoạch khám bệnh bác sĩ 
                 name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
+            },
+            { //Quản lý bệnh nhân khám bệnh của bác sĩ 
+                name: 'menu.doctor.manage-patient', link: '/doctor/manage-patient'
             },
         ]
     }
