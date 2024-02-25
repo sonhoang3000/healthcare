ce38ab09df89e8e6fd452a17fe613b0ca39a2b01
sonhoang3000 <vuhoangson3000@gmail.com>
2024-02-25 13:56:17 +0700

    #83

--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userController from "../controllers/userController";
+import doctorController from "../controllers/doctorController"
 let router = express.Router();
 
 let initWebRoutes = (app) => {
@@ -10,6 +11,15 @@ let initWebRoutes = (app) => {
   router.put("/api/edit-user", userController.handleEditUser)
   router.delete("/api/delete-user", userController.handleDeleteUser) // rest API
   router.get('/api/allcode', userController.getAllCode);
+
+  router.get('/api/top-doctor-home', doctorController.getTopDoctorHome);
+  router.get('/api/get-all-doctors', doctorController.getAllDoctors);
+  router.post('/api/save-infor-doctors', doctorController.postInforDoctor);
+  router.get('/api/get-detail-doctor-by-id', doctorController.getDetailDoctorById);
+  router.post('/api/bulk-create-schedule', doctorController.bulkCreateSchedule);
+  router.get('/api/get-schedule-doctor-by-date', doctorController.getScheduleByDate);
+  router.get('/api/get-extra-infor-by-id', doctorController.getExtraInforById);
+  router.get('/api/get-profile-doctor-by-id', doctorController.getProfileDoctorById);
 
   return app.use("/", router);
 };
--- src/server.js
+++ src/server.js
@@ -40,7 +40,7 @@ app.use(function (req, res, next) {
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb' }, { extended: true }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
